fda99dc899974e4d1add6fab87581ee6496a5208
Kyaw <kyawzayartun0527@gmail.com>
2025-01-30 01:52:04 +0630

    chore: import Remix and Font Awesome CDN in CSS and remove URL from HTML for cleaner structure

--- Store/StoreCheckout.php
+++ Store/StoreCheckout.php
@@ -23,7 +23,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['order'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Opulence Haven</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" integrity="sha512-HXXR0l2yMwHDrDyxJbrMD9eLvPe3z3qL3PPeozNTsiHJEENxx8DH2CxmV05iwG0dwoz5n4gQZQyYLUNt1Wdgfg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../CSS/output.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../CSS/input.css?v=<?php echo time(); ?>">
 </head>
--- User/ForgetPassword.php
+++ User/ForgetPassword.php
@@ -10,7 +10,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Opulence Haven</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.css" integrity="sha512-HXXR0l2yMwHDrDyxJbrMD9eLvPe3z3qL3PPeozNTsiHJEENxx8DH2CxmV05iwG0dwoz5n4gQZQyYLUNt1Wdgfg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="CSS/output.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="CSS/input.css?v=<?php echo time(); ?>">
 </head>
